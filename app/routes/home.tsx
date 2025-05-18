fb34c15928fe5c45da6bfaddb127230b2b91b5da
Mike Miller <mikej.miller440@gmail.com>
2025-05-18 18:57:27 -0500

    added shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,15 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'Carol Trainor' },
+    {
+      name: 'description',
+      content: 'A memorial website for Carol Anne Trainor'
+    }
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div></div>
 }
