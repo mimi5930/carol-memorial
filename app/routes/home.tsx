b8f305fdde58cbd9c70580378270fc079dd55320
Mike Miller <mikej.miller440@gmail.com>
2025-05-20 17:56:44 -0500

    changes to landing page content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,6 +10,10 @@ import {
 import { carolHeadshot, headerLilacImg } from '~/assets'
 import { Button } from '~/components/ui/button'
 import { Link } from 'react-router'
+import CalendarHeart from '~/components/svg/calendarHeart'
+import Clock from '~/components/svg/clock'
+import FacebookIcon from '~/components/svg/FacebookIcon'
+import { Separator } from '~/components/ui/separator'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -90,10 +94,10 @@ export default function Home() {
         <img
           src={headerLilacImg}
           alt=""
-          className="w-full h-full object-cover object-top border-b-8 border-amber-400"
+          className="w-full h-full object-cover object-top border-b-8 border-amber-400 shadow-lg"
         />
         <div className="absolute bottom-0 left-0 h-full w-full flex flex-col justify-center items-center">
-          <div className="flex flex-col justify-center items-center rounded-xl bg-accent/55 p-8 shadow-lg">
+          <div className="flex flex-col justify-center items-center rounded-xl bg-accent/70 p-8 shadow-lg">
             <div className="flex justify-center items-center gap-8">
               <h2 className="font-ephesis text-6xl font-extrabold text-orange-200 text-shadow-md text-shadow-black">
                 1966
@@ -120,7 +124,120 @@ export default function Home() {
         </div>
       </header>
       <main className="py-12 flex justify-center">
-        <section className="p-12 flex flex-col items-center w-7xl gap-6 bg-slate-100 rounded-xl shadow-md text-lg/8">
+        <section className="flex justify-evenly w-full ">
+          <div className="w-5xl bg-slate-100 rounded-xl shadow-md text-lg/8 p-12 flex flex-col gap-24">
+            <div className="">
+              <h2 className="text-4xl font-bold pb-4">
+                A Personal Message from Carol's Family
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga,
+                nulla. Minima in mollitia nisi. Eius, nostrum in laborum,
+                repellendus nesciunt dolorem beatae, magnam animi accusantium
+                eaque cupiditate saepe illum. Aperiam repellendus aliquam
+                dolorem neque at illum autem asperiores, corrupti officiis.
+              </p>
+            </div>
+            <Separator />
+            <div>
+              <h2 className="text-4xl font-bold pb-4">
+                Celebrate Carol's Life
+              </h2>
+              <h2 className="text-2xl font-bold pb-4">Visitations</h2>
+              <div className="flex gap-8">
+                <div className="bg-lilac p-8 shadow-sm rounded-md flex flex-col gap-1">
+                  <div className="flex items-center gap-2">
+                    <CalendarHeart />
+                    <h3 className="text-lg font-bold">Friday, June 20, 2025</h3>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Clock />
+                    <h3>2PM - 6PM</h3>
+                  </div>
+                  <div>
+                    <p>
+                      <a
+                        href="https://www.oconnellfuneralhomes.com/"
+                        className="underline"
+                      >
+                        O'Connell Family Funeral Home
+                      </a>
+                    </p>
+                    <p className="text-sm">
+                      520 11th Street South Hudson, WI 54016
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-2 pt-2">
+                    <Button>
+                      <a href="https://www.facebook.com/share/1GgRLGXKA3/">
+                        <FacebookIcon />
+                      </a>
+                    </Button>
+                    <Button>Add to Calendar</Button>
+                  </div>
+                </div>
+                <div className="bg-lilac p-8 shadow-sm rounded-md flex flex-col gap-1">
+                  <div className="flex items-center gap-2">
+                    <CalendarHeart />
+                    <h3 className="text-lg font-bold">
+                      Saturday, June 21, 2025
+                    </h3>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Clock />
+                    <h3>11AM - 3PM</h3>
+                  </div>
+                  <div>
+                    <p>
+                      <a
+                        href="https://www.oconnellfuneralhomes.com/"
+                        className="underline"
+                      >
+                        O'Connell Family Funeral Home
+                      </a>
+                    </p>
+                    <p className="text-sm">
+                      520 11th Street South Hudson, WI 54016
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-2 pt-2">
+                    <Button>
+                      <a href="https://www.facebook.com/share/1LwVhF7NPC/">
+                        <FacebookIcon />
+                      </a>
+                    </Button>
+                    <Button>Add to Calendar</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Separator />
+            <div className="flex flex-col gap-4">
+              <h2 className="text-4xl font-bold">Obituary</h2>
+              <p className="self-start">
+                Carol A. Trainor, age 58, of Hudson, Wisconsin, passed away on
+                May 5, 2025. Carol was born October 20, 1966 in St. Paul, MN to
+                Richard and Louise (Thompson) Weiher. She married Chad Trainor
+                on September 1, 1993 in Milwaukee, WI...
+              </p>
+              <Button>Go to full Obituary</Button>
+            </div>
+          </div>
+          <div className="w-3xl bg-slate-100 shadow-md text-lg/8 p-12 gap-8 flex flex-col h-max">
+            <h2 className="text-4xl font-bold pb-4">In Lieu of Flowers</h2>
+            <p>
+              Carol's family asks for you to please make a donation to Carol's
+              memorial fund.
+            </p>
+            <p>
+              This fund has been established to extend the love help our family
+              has received to others who may be experiencing such loss or
+              hardships.
+            </p>
+            <Button>Carol's Memorial Fund</Button>
+          </div>
+        </section>
+        {/* <section className="p-12 flex flex-col items-center w-7xl gap-6 bg-slate-100 rounded-xl shadow-md text-lg/8">
           <h2 className="font-ephesis text-7xl font-extrabold py-2">
             Carol's Life
           </h2>
@@ -183,12 +300,12 @@ export default function Home() {
             <Button variant="secondary">Download Carol's Obituary</Button>
             <Button>Carol's Memorial Fund</Button>
           </div>
-        </section>
+        </section> */}
       </main>
-      <footer className="h-[30rem] bg-slate-500 flex flex-col justify-center items-center text-base text-orange-50">
+      <footer className="h-[25rem] bg-slate-500 flex flex-col justify-center items-center text-base text-orange-50">
         <div>
           <h2 className="font-bold text-2xl pb-3">Carol Ann Trainor</h2>
-          <div className="flex gap-16">
+          <div className="flex gap-32">
             <div className="flex flex-col gap-2">
               <h3 className="font-bold text-lg">ABOUT</h3>
               <Link
