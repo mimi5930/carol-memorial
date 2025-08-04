209d9a524aa0d4b11f57b07c7c1986cff9dec511
Mike Miller <mikej.miller440@gmail.com>
2025-08-04 16:05:26 -0500

    added gallery and carousel

--- app/routes/gallery.tsx
+++ app/routes/gallery.tsx
@@ -1,3 +1,38 @@
-export default function gallery() {
-  return <div>gallery</div>
+import { carolHeadshot } from '~/assets'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '~/components/ui/carousel'
+
+export default function CarouselDemo() {
+  return (
+    <section className="flex flex-col items-center py-12">
+      <h1 className="font-ephesis text-7xl font-extrabold py-12">
+        Remember Carol With Us
+      </h1>
+      <div className="bg-maroon p-16 rounded-md shadow-lg">
+        <div className="w-7xl flex justify-center items-center ">
+          <Carousel className="w-full max-w-2xl">
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="basis-3/4">
+                  <div className="p-1 bg-amber-50">
+                    <img src={carolHeadshot} alt="" />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </div>
+        <p className="text-center pt-12 text-slate-50 underline">
+          View more pictures
+        </p>
+      </div>
+    </section>
+  )
 }
