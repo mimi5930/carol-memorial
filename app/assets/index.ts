1fe75cb556d368a6b64421d22a58f92e1f9fa46e
Mike Miller <mikej.miller440@gmail.com>
2025-10-19 22:03:25 -0500

    added flowery assets

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,5 +1,17 @@
 import headerLilacImg from './header-lilac.jpg'
 import carolHeadshot from './carol-headshot.jpg'
 import hydrangeas from './hydrangeas.jpg'
+import flowerHeart from './flower-heart.png'
+import sunFlowers from './sunflowers.png'
+import flowerBorder from './flower-border.png'
+import purpleFlowers from './purple-flowers.png'
 
-export { headerLilacImg, carolHeadshot, hydrangeas }
+export {
+  headerLilacImg,
+  carolHeadshot,
+  hydrangeas,
+  flowerHeart,
+  sunFlowers,
+  flowerBorder,
+  purpleFlowers
+}
