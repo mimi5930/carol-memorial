980b7beba405699fa6c8cfedca0a103c162e61eb
Mike Miller <mikej.miller440@gmail.com>
2025-10-19 22:04:00 -0500

    created component for section headers

---
+++ app/components/PageHeader.tsx
@@ -0,0 +1,25 @@
+import { flowerBorder } from '~/assets'
+
+export default function PageHeader({
+  pageTitle,
+  ...props
+}: React.ComponentPropsWithoutRef<'header'> & { pageTitle: string }) {
+  return (
+    <header
+      className="h-[28rem] relative border-b-[1rem] border-maroon"
+      {...props}
+    >
+      <img
+        src={flowerBorder}
+        alt=""
+        className="w-full h-full object-cover object-bottom shadow-lg opacity-30"
+      />
+      <div className="w-full h-full absolute top-0 right-0 bg-gradient-to-b from-lilac to-transparent to-80%" />
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        <h1 className="font-ephesis text-5xl px-4 text-center md:text-9xl font-extrabold text-sandy-brown text-shadow-md text-shadow-black">
+          {pageTitle}
+        </h1>
+      </div>
+    </header>
+  )
+}
